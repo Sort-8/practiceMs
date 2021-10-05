0e854f177f0dc57cb8b0b3a2d1dba65a1d2d058e
hw57584513 <1449430837@qq.com>
2021-10-05 22:50:04 +0800

    完成User接口文档

--- ruoyi-common/src/main/java/com/ruoyi/common/core/domain/entity/SysUser.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/domain/entity/SysUser.java
@@ -7,6 +7,8 @@ import com.ruoyi.common.annotation.Excel.ColumnType;
 import com.ruoyi.common.annotation.Excel.Type;
 import com.ruoyi.common.annotation.Excels;
 import com.ruoyi.common.core.domain.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -21,63 +23,80 @@ import java.util.List;
  *
  * @author tomorrow
  */
+@ApiModel(value = "SysUser", description = "用户实体")
 public class SysUser extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
 
     /** 用户ID */
+    @ApiModelProperty("用户ID")
     @Excel(name = "用户序号", cellType = ColumnType.NUMERIC, prompt = "用户编号")
     private Long userId;
 
     /** 部门ID */
+    @ApiModelProperty("专业ID")
     //@Excel(name = "部门编号", type = Type.IMPORT)
     private Long deptId;
 
     /** 用户账号 */
+    @ApiModelProperty("用户名")
     @Excel(name = "登录名称")
     private String userName;
 
     /** 用户昵称 */
+    @ApiModelProperty("用户昵称")
     @Excel(name = "用户名称")
     private String nickName;
 
     /** 用户邮箱 */
+    @ApiModelProperty("用户邮箱")
     @Excel(name = "用户邮箱")
     private String email;
 
     /** 手机号码 */
+    @ApiModelProperty("手机号码")
     @Excel(name = "手机号码")
     private String phonenumber;
 
     /** 用户性别 */
+    @ApiModelProperty("用户性别")
     @Excel(name = "用户性别", readConverterExp = "0=男,1=女,2=未知")
     private String sex;
 
     /** 用户头像 */
+    @ApiModelProperty(hidden = true)
+//    @ApiModelProperty("用户头像")
     private String avatar;
 
-    /** 密码 */
+    /** 密码 **/
+    @ApiModelProperty("用户密码")
     private String password;
 
     /** 盐加密 */
+    @ApiModelProperty("盐加密")
     private String salt;
 
     /** 帐号状态（0正常 1停用） */
+    @ApiModelProperty("帐号状态（0正常 1停用）")
     @Excel(name = "帐号状态", readConverterExp = "0=正常,1=停用")
     private String status;
 
     /** 删除标志（0代表存在 2代表删除） */
+    @ApiModelProperty("删除标志（0代表存在 2代表删除）")
     private String delFlag;
 
     /** 最后登录IP */
+    @ApiModelProperty("最后登录IP")
     @Excel(name = "最后登录IP", type = Type.EXPORT)
     private String loginIp;
 
     /** 最后登录时间 */
+    @ApiModelProperty("最后登录时间")
     @Excel(name = "最后登录时间", width = 30, dateFormat = "yyyy-MM-dd HH:mm:ss", type = Type.EXPORT)
     private Date loginDate;
 
     /** 部门对象 */
+    @ApiModelProperty("专业对象")
     @Excels({
         @Excel(name = "专业", targetAttr = "deptName", type = Type.EXPORT),
         @Excel(name = "专业负责人", targetAttr = "leader", type = Type.EXPORT)
@@ -85,15 +104,19 @@ public class SysUser extends BaseEntity
     private SysDept dept;
 
     /** 角色对象 */
+    @ApiModelProperty("角色对象")
     private List<SysRole> roles;
 
     /** 角色组 */
+    @ApiModelProperty("角色组")
     private Long[] roleIds;
 
     /** 岗位组 */
+    @ApiModelProperty(hidden = true)
     private Long[] postIds;
 
     /** 角色ID */
+    @ApiModelProperty("角色ID")
     private Long roleId;
 
     public SysUser()
